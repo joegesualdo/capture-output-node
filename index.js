ca591dea7a690543421b8a9a98af68bda352c026
Joe Gesualdo <joegesualdo@gmail.com>
2016-06-25 21:26:47 -0400

    Add code

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-"use strict";
+import fs from 'fs';
+import mkdirp from 'mkdirp';
+
+export default class CaptureOutput {
+  constructor() {
+    if (!fs.existsSync(__dirname + '/tmp')){
+      fs.mkdirSync(__dirname + '/tmp');
+    }
+    let fileName = __dirname + '/tmp/captured-ouput.txt';
+    this.originalStdoutWrite = process.stdout.write
+    this.originalStderrWrite = process.stderr.write
+    this.fileName = fileName;
+    var access = fs.createWriteStream(fileName);
+    process.stdout.write = process.stderr.write = access.write.bind(access);
+    process.on('unhandledRejection', (reason) => {
+      this.stop()
+      throw new Error('error');
+    });
+  }
+  get() {
+    return new Promise(resolve => {
+      var readStream = fs.createReadStream(this.fileName);
+      let file = '';
+      readStream.on('data', (chunk) => {
+        file += chunk.toString('utf8');
+      })
+      readStream.on('end', () => {
+        resolve(file)
+      })
+    })
+  }
+  stop() {
+    return new Promise(resolve => {
+      process.stdout.write = this.originalStdoutWrite;
+      process.stderr.write = this.originalStderrWrite;
+      resolve();
+    })
+  }
+}
---
+++ readme.md
@@ -0,0 +1,51 @@
+## @joegesualdo/capture-output-node [![Build Status](https://travis-ci.org/joegesualdo/capture-output-node.svg?branch=master)](https://travis-ci.org/joegesualdo/capture-output-node)
+> Capture console.log output. Useful for testing cli modules.
+
+## Highlights
+
+- Highlight 1
+- Highlight 2
+- Highlight 3
+
+## Install
+```
+$ npm install --save @joegesualdo/capture-output-node 
+```
+
+## Usage
+```javascript
+var @joegesualdo/captureOutputNode = require("@joegesualdo/capture-output-node").default
+
+// insert code example here
+```
+
+## Test
+```
+$ npm test
+```
+## API
+### `methodName(arg1, arg2)`
+> What does this method do?
+
+| Name | Type | Description |
+|------|------|-------------|
+| arg1 | `Array` | Test description|
+| arg2 | `String` | Test description|
+
+Returns: `Array`, of things
+
+```javascript
+var @joegesualdo/captureOutputNode = require("@joegesualdo/capture-output-node").default
+
+// insert method example here
+```
+## Build
+```
+$ npm run build
+```
+
+## Related
+- [example-package]() - Add description of the example package here.
+
+## License
+MIT © [Joe Gesualdo]()
